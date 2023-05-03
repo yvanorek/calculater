9b8a3cad7ba69c16c69fd3e641442ff5e6189892
VANOREK Yessica <yvanorek@vbs>
2023-05-03 10:28:32 +0200

    initial commit

---
+++ src/main/java/com/example/swlc/SwlcApplication.java
@@ -0,0 +1,13 @@
+package com.example.swlc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwlcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwlcApplication.class, args);
+    }
+
+}
