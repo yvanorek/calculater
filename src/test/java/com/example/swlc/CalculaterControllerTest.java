825f33f89eab07463079fd7f14a6948c6b336579
VANOREK Yessica <yvanorek@vbs>
2023-05-03 11:49:35 +0200

    calculator controller

---
+++ src/test/java/com/example/swlc/CalculaterControllerTest.java
@@ -0,0 +1,56 @@
+package com.example.swlc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculaterControllerTest {
+
+    private CalculaterController calculatorController =new CalculaterController();
+
+    @Test
+    public void sumTest1(){
+        //arrange
+        int a=6;
+        int b=11;
+        //act
+        int result =calculatorController.sum(a,b);
+        //assert
+        assertEquals(17, result);
+    }
+
+    @Test
+    public void sumTest2(){
+        //arrange
+        int a=-6;
+        int b=11;
+        //act
+        int result =calculatorController.sum(a,b);
+        //assert
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void sumTest3(){
+        //arrange
+        int a=-6;
+        int b=-11;
+        //act
+        int result =calculatorController.sum(a,b);
+        //assert
+        assertEquals(-17, result);
+    }
+
+    @Test
+    public void sumTest4(){
+        //arrange
+        int a=6;
+        int b=-11;
+        //act
+        int result =calculatorController.sum(a,b);
+        //assert
+        assertEquals(-5, result);
+    }
+
+
+}
