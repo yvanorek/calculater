e1dc5b99d17fe6d7810294d7332f1ac8b81d84e2
VANOREK Yessica <yvanorek@vbs>
2023-05-17 15:27:40 +0200

    minus funktion

--- src/main/java/com/example/swlc/CalculaterController.java
+++ src/main/java/com/example/swlc/CalculaterController.java
@@ -19,6 +19,13 @@ public class CalculaterController {
         return a+b;
     }
 
+    @RequestMapping("/calc/sum")                                  // mit @RequestMapping ändert/erstellt man den Pfad der Webseite -->http://localhost:8080/calc/sum
+    public int minus (@RequestParam int a, @RequestParam int b){      // http://localhost:8080/calc/sum?a=3&b=4
+        return a-b;
+    }
+
+
+
 
 
 
